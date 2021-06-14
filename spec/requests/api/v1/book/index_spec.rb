fd9d9c12c0f49da5e346cca3a9bef9cb03fc8cdb
brueck1988 <brueck1988@gmail.com>
2021-06-14 10:27:48 -0600

    Request spec created

--- spec/requests/api/v1/book/index_spec.rb
+++ spec/requests/api/v1/book/index_spec.rb
@@ -7,87 +7,30 @@ RSpec.describe 'Forecast API', :vcr do
     books = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-# {
-#   "data": {
-#     "id": "null",
-#     "type": "books",
-#     "attributes": {
-#       "destination": "denver,co",
-#       "forecast": {
-#         "summary": "Cloudy with a chance of meatballs",
-#         "temperature": "83 F"
-#       },
-#       "total_books_found": 172,
-#       "books": [
-#         {
-#           "isbn": [
-#             "0762507845",
-#             "9780762507849"
-#           ],
-#           "title": "Denver, Co",
-#           "publisher": [
-#             "Universal Map Enterprises"
-#           ]
-#         },
-#         {
-#           "isbn": [
-#             "9780883183663",
-#             "0883183668"
-#           ],
-#           "title": "Photovoltaic safety, Denver, CO, 1988",
-#           "publisher": [
-#             "American Institute of Physics"
-#           ]
-#         },
-#         { ... same format for books 3, 4 and 5 ... }
-#       ]
-#     }
-#   }
-# }
-    # expect(books[:data]).to be_a(Hash)
-    # expect(books[:data][:id]).to be(nil)
-    # expect(books[:data][:type]).to eq("book")
-    # 
-    # expect(books[:data][:attributes]).to have_key(:current_weather)
-    # expect(books[:data][:attributes]).to have_key(:daily_weather)
-    # expect(books[:data][:attributes]).to have_key(:hourly_weather)
-    # expect(books[:data][:attributes]).to_not have_key(:minutely)
-    # expect(books[:data][:attributes]).to_not have_key(:alerts)
-    # 
-    # expect(books[:data][:attributes][:current_weather]).to be_a(Hash)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:datetime)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:sunrise)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:sunset)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:temperature)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:feels_like)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:humidity)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:uvi)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:visibility)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:conditions)
-    # expect(books[:data][:attributes][:current_weather]).to have_key(:icon)
-    # 
-    # expect(books[:data][:attributes][:daily_weather]).to be_an(Array)
-    # expect(books[:data][:attributes][:daily_weather].length).to eq(5)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:date)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:sunrise)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:sunset)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:max_temp)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:min_temp)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:conditions)
-    # expect(books[:data][:attributes][:daily_weather][0]).to have_key(:icon)
-    # 
-    # expect(books[:data][:attributes][:hourly_weather]).to be_a(Array)
-    # expect(books[:data][:attributes][:hourly_weather].length).to eq(8)
-    # expect(books[:data][:attributes][:hourly_weather][0]).to have_key(:time)
-    # expect(books[:data][:attributes][:hourly_weather][0]).to have_key(:temperature)
-    # expect(books[:data][:attributes][:hourly_weather][0]).to have_key(:conditions)
-    # expect(books[:data][:attributes][:hourly_weather][0]).to have_key(:icon)
-  end
 
-  xit 'sad path, bad search entry returns 404' do
-    # get "/api/v1/book?location="
-    # 
-    # expect(response).to_not be_successful
-    # expect(response.status).to eq(404)
+    expect(books[:data]).to be_a(Hash)
+    expect(books[:data][:id]).to be(nil)
+    expect(books[:data][:type]).to eq("books")
+    
+    expect(books[:data][:attributes]).to have_key(:destination)
+    expect(books[:data][:attributes][:destination]).to be_a(String)
+    expect(books[:data][:attributes]).to have_key(:destination_forecast)
+    expect(books[:data][:attributes][:destination_forecast]).to be_a(Hash)
+    expect(books[:data][:attributes][:destination_forecast]).to have_key(:summary) #conditions
+    expect(books[:data][:attributes][:destination_forecast][:summary]).to be_a(String)
+    expect(books[:data][:attributes][:destination_forecast]).to have_key(:temperature) #temperature
+    expect(books[:data][:attributes][:destination_forecast][:temperature]).to be_a(String) #temperature
+
+    expect(books[:data][:attributes]).to have_key(:total_books_found)
+    expect(books[:data][:attributes][:total_books_found]).to be_an(Integer)
+    expect(books[:data][:attributes]).to have_key(:books)
+    expect(books[:data][:attributes][:books]).to be_an(Array)
+    expect(books[:data][:attributes][:books].count).to eq(5)
+    expect(books[:data][:attributes][:books][0]).to be_a(Hash)
+    expect(books[:data][:attributes][:books][0][:isbn]).to be_an(Array)
+    expect(books[:data][:attributes][:books][0][:isbn][0]).to be_a(String)
+    expect(books[:data][:attributes][:books][0][:title]).to be_a(String)
+    expect(books[:data][:attributes][:books][0][:publisher]).to be_an(Array)
+    expect(books[:data][:attributes][:books][0][:publisher][0]).to be_a(String)
   end
 end
