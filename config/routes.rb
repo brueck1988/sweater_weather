bb832d70729e94d290dd637e31d53cef3ee38c1f
brueck1988 <brueck1988@gmail.com>
2021-06-14 09:19:47 -0600

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/api/v1/book-search', to: 'book#index'
+  
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
-      resources :book, only: [:index]
     end
   end
 end
+
