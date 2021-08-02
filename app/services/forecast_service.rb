a6acf881ab318c3a108b457b57cd21148b481c91
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-02 14:27:34 -0600

    Refactor image service

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -19,3 +19,19 @@ class ForecastService
     JSON.parse(response.body, symbolize_names: true)
   end
 end
+
+# def self.get_roadtrip(origin, destination)
+#   response = conn.get("/directions/v2/route") do |faraday|
+#     faraday.params['from'] = origin
+#     faraday.params['to'] = destination
+#   end
+#   parse_json(response)
+# end
+# 
+# private
+# 
+# def self.conn
+#   Faraday.new(url: 'http://www.mapquestapi.com') do |faraday|
+#     faraday.params['key'] = ENV['key']
+#   end
+# end
