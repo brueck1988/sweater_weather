d698ae23fb7a81fbf935130bc5bb85917a8883fe
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-08-02 14:30:18 -0600

    Refactor forecast service

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,17 +1,19 @@
 class ForecastService
   def self.get_forecasts(coordinates)
-    parse_json(conn(coordinates).get("/data/2.5/onecall"))
-  end
-  
-  private
-  
-  def self.conn(coordinates)
-    Faraday.new(url: 'https://api.openweathermap.org') do |faraday|
-      faraday.params['appid'] = ENV['appid']
+    response = conn.get("/data/2.5/onecall") do |faraday|
       faraday.params['lat'] = coordinates.latitude
       faraday.params['lon'] = coordinates.longitude
       faraday.params['exclude'] = "minutely,alerts"
       faraday.params['units'] = "imperial"
+    end
+    parse_json(response)
+  end
+  
+  private
+  
+  def self.conn
+    Faraday.new(url: 'https://api.openweathermap.org') do |faraday|
+      faraday.params['appid'] = ENV['appid']
     end
   end
   
@@ -19,19 +21,3 @@ class ForecastService
     JSON.parse(response.body, symbolize_names: true)
   end
 end
-
-# def self.get_roadtrip(origin, destination)
-#   response = conn.get("/directions/v2/route") do |faraday|
-#     faraday.params['from'] = origin
-#     faraday.params['to'] = destination
-#   end
-#   parse_json(response)
-# end
-# 
-# private
-# 
-# def self.conn
-#   Faraday.new(url: 'http://www.mapquestapi.com') do |faraday|
-#     faraday.params['key'] = ENV['key']
-#   end
-# end
